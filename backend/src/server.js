481b54074ca1274a50dddd27e00b4ae00f23a609
Allan Cavalcante <149847141+allan141@users.noreply.github.com>
2024-10-30 12:22:31 -0300

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,28 +1,16 @@
-// Importação da classe WebSocketServer da biblioteca "ws"
-const { WebSocketServer } = require("ws");
+const { WebSocketServer } = require("ws")
+const dotenv = require("dotenv")
 
-// Importação da biblioteca dotenv para carregar variáveis de ambiente
-const dotenv = require("dotenv");
+dotenv.config()
 
-// Carregamento das variáveis de ambiente do arquivo .env
-dotenv.config();
+const wss = new WebSocketServer({ port: process.env.PORT || 8080 })
 
-// Criação de um novo servidor WebSocket na porta especificada pela variável de ambiente PORT ou 8080
-const wss = new WebSocketServer({ 
-  port: process.env.PORT || 8080 
-});
-
-// Definição do evento de conexão do servidor WebSocket
 wss.on("connection", (ws) => {
-  // Definição do evento de erro do cliente WebSocket
-  ws.on("error", console.error);
-  
-  // Definição do evento de mensagem recebida do cliente WebSocket
-  ws.on("message", (data) => {
-    // Envio da mensagem recebida para todos os clientes conectados
-    wss.clients.forEach((client) => client.send(data.toString()));
-  });
-  
-  // Registro da conexão de um novo cliente
-  console.log("Cliente conectado");
-});
+    ws.on("error", console.error)
+
+    ws.on("message", (data) => {
+        wss.clients.forEach((client) => client.send(data.toString()))
+    })
+
+    console.log("client connected")
+})
